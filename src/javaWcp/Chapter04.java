a9119f17fea119a89f9296a160293978ecdb0227
user <user@192.168.3.16>
2022-10-31 19:40:16 +0900

    変数&定数

---
+++ src/javaWcp/Chapter04.java
@@ -0,0 +1,15 @@
+package javaWcp;
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "hashi";
+		System.out.println(name);
+		
+		name = "power!";
+		System.out.println(name);
+		
+		final int LUCKY_NUMBER = 9;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
+
