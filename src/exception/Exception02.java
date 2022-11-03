cb0926c3a89c94dcff6a699d7f18398ca231df09
user <user@192.168.3.16>
2022-11-03 21:01:08 +0900

    [Fix]例外

--- src/exception/Exception02.java
+++ src/exception/Exception02.java
@@ -4,7 +4,7 @@ public class Exception02 {
 	public static void main(String[] args) {
 		try {
 			// 例外が発生する可能性がある処理を記述
-			System.out.println("100 / 0は？");
+			System.out.println("100 / 0 は？");
 			int result = 100 / 0;
 			System.out.println("計算結果" + result);
 			// ArithmeticExceptionクラスの例外が発生した場合の例外処理
