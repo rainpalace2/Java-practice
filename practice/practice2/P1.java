e57eb6d8d695f0c2b859b8bacca5edecc4a136b2
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-11-15 14:44:39 +0900

    [Add]array

---
+++ practice/practice2/P1.java
@@ -0,0 +1,17 @@
+package practice2;
+
+// 標準入力とループ処理
+import java.util.Scanner;
+
+public class P1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+
+    String player;
+    for (int i = 1; i <= number; i++) {
+      player = sc.next();
+      System.out.println(player);
+    }
+  }
+}
---
+++ practice/practice2/array.java
@@ -0,0 +1,19 @@
+package practice2;
+
+public class array {
+  public static void main(String[] args) {
+    String player1 = "勇者";
+    String player2 = "騎士";
+    String player3 = "戦士";
+
+    System.out.println(player1);
+    System.out.println(player2);
+
+    // 配列の要素を変数で指定
+    // 文字列の配列に数値は入れられない　100× "100"○
+    String[] team = { "勇者", "騎士", player3 };
+    System.out.println(team[0]);
+    System.out.println(team[1]);
+    System.out.println(team[2]);
+  }
+}
