f68227ab8daf954cb0be1da51d16136a471cdaa7
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-11-24 16:05:10 +0900

    [Add]特殊文字

---
+++ practice/practice2/moji.java
@@ -0,0 +1,11 @@
+package practice2;
+
+// \n 改行
+// \t タブ
+
+public class moji {
+  public static void main(String[] args) {
+    String msg = "Hello Wo\nrld Again\t!";
+    System.out.println(msg);
+  }
+}
