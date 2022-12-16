0134afaec48b1504ea78303348256086e0e0fa8f
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2022-12-16 15:55:54 +0900

    [Add]half,right_triangle

---
+++ practice/practice5/half.java
@@ -0,0 +1,23 @@
+package practice5;
+
+import java.util.Scanner;
+
+// n個の文字を半分ずつ出力
+
+public class half {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int s = sc.nextInt();
+
+    for (int i = 1; i <= s; i++) {
+      System.out.println(i);
+      // 1行目には1以上n/2以下の数を、2行目にはn/2+1以上n以下の数を出力する
+      if (i == s / 2 || i == s) {
+        System.out.println();
+      } else {
+        System.out.println(" ");
+      }
+    }
+  }
+}
---
+++ practice/practice5/right_triangle.java
@@ -0,0 +1,28 @@
+package practice5;
+
+import java.util.Scanner;
+
+// 直角三角形の出力
+// 1 行目の出力する個数は 1 つ、2 行目の出力する個数は 2 つ。i 行目の出力する個数は i 個。
+
+public class right_triangle {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    // 外側のforは行を、内側のforは列をを表すと考える
+    // i 行で出力する値は 1 ~ i までの整数なので、内側のfor文は j=1から j=iまでの繰り返し
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.println(j);
+        if (j == i) {
+          System.out.println();
+        } else {
+          System.out.print(" ");
+        }
+      }
+    }
+  }
+}
