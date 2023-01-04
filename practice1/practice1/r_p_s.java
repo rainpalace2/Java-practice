19da66f594f911152be69611c861f28334675a5a
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2023-01-04 18:31:01 +0900

    [Add]ゾロ目

---
+++ practice1/practice1/r_p_s.java
@@ -0,0 +1,25 @@
+package practice1;
+
+import java.util.Scanner;
+
+// じゃんけんで必ず勝つ
+
+public class r_p_s {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    // char型を標準入力から受け取る
+    char c = sc.next().charAt(0);
+
+    // char型はプリミティブ型なので == で比較できる
+    // ダブルクォーテーションではなくシングルクォーテーションであることに注意
+    if (c == 'R') {
+      System.out.println('P');
+    } else if (c == 'P') {
+      System.out.println('S');
+    } else {
+      System.out.println('R');
+    }
+  }
+}
---
+++ practice1/practice2/parallel_eyes.java
@@ -0,0 +1,16 @@
+package practice2;
+
+import java.util.Scanner
+
+// ゾロ目
+
+public class parallel_eyes {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    System.out.println(n / 11 * 11);
+  }
+}
