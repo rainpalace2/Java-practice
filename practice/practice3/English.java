b67978bf3e2e16bc22a0c02dcd29ac15a85333d7
rainpalace2 <110389642+rainpalace2@users.noreply.github.com>
2023-01-25 13:40:47 +0900

    [Fix]

--- practice/practice3/English.java
+++ practice/practice3/English.java
@@ -2,7 +2,7 @@ package practice3;
 
 // 文字列を、splitメソッドを使って、スペースのところで分割して、単語の数を出力する。
 
-public class english {
+public class English {
   public static void main(String[] args) {
     String str = "One cold rainy day when my father was a little boy he met an old alley cat on his street";
     // 文字列を.split(" ")で分割し、要素数をlengthメソッドで出力する
--- practice/practice3/Expansion.java
+++ practice/practice3/Expansion.java
@@ -1,6 +1,6 @@
 package practice3;
 
-public class expansion {
+public class Expansion {
   public static void main(String[] args) {
     String[] team = { "勇者", "戦士", "魔法使い", "忍者" };
     // team配列から要素を一つずつ取り出してmember変数に代入している
